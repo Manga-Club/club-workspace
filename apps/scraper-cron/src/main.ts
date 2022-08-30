54f4604de1933d0d1dc205c1cf86a898a98f4d9e
Kassio Khaleb <kassio_khaleb01@hotmail.com>
2022-08-30 10:12:06 -0300

    feat: develop script

--- apps/scraper-cron/src/main.ts
+++ apps/scraper-cron/src/main.ts
@@ -1,9 +1,31 @@
 import { getAllChapters } from './app/chapters';
 import { verifyComics } from './app/comics';
+var term = require('terminal-kit').terminal;
 
-(async () => {
-  //await verifyComics();
-  await getAllChapters(
-    'https://neoxscans.net/manga/the-world-after-the-end-novel/'
-  );
-})();
+term.cyan('Welcome to the debug tool.\n');
+term.cyan('Pick a script.\n');
+
+var items = ['1. Verify New Comics', '2. Get All Chapters'];
+const functions = [verifyComics, getAllChapters];
+
+const runOptions = () => {
+  term.singleColumnMenu(items, async (err, response) => {
+    if (err) {
+      term('\n').eraseLineAfter.red('#Error - %s\n', err.message);
+      return;
+    }
+
+    term('\n').eraseLineAfter.green('#Selected - %s\n', response.selectedText);
+    2;
+
+    const currentFunction = functions[response.selectedIndex];
+
+    // In the future this will be dynamic
+    await currentFunction(
+      'https://neoxscans.net/manga/the-world-after-the-end-novel/'
+    );
+    process.exit();
+  });
+};
+
+runOptions();
